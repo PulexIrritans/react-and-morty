ae411fe8d6e3d43429d67a9698915bec13c266cd
Antje Exner <antjeexner@Antjes-Air.fritz.box>
2022-04-05 14:59:55 +0200

    add fetch api but only first page of api

--- src/App.js
+++ src/App.js
@@ -2,20 +2,40 @@ import './App.css';
 import Header from './Components/Header';
 import Card from './Components/Card';
 import Footer from './Components/Footer';
+import { useEffect, useState } from "react";
+
+  
 
 
 function App() {
+
+  const [characters, setCharacters] = useState([]);
+
+  const url = "https://rickandmortyapi.com/api/character/";
+
+  const loadCharacters = () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setCharacters(data.results));
+  };
+
+  useEffect(() => {
+    loadCharacters();
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <main>
         <ul className="Cards-Container">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        {characters
+            .map((character) => (
+              <Card
+                key={character.id}
+                image={character.image}
+                name={character.name}
+              />
+            ))}
         </ul>
       </main>
       <Footer />
---
+++ src/Components/main.js
@@ -0,0 +1,100 @@
+// API Fetch
+const baseUrl = "https://rickandmortyapi.com/api/character?page=";
+const numPages = 5;
+
+const urls = Array(numPages)
+  .fill() // [undefined, undefined, ...]
+  .map((_, index) => baseUrl + (index + 1));
+
+const promises = urls.map((url) => fetch(url).then((res) => res.json()));
+
+// Global variables and constants
+const loadDataButtonElement = document.querySelector(".header-btn");
+const cardContainerElement = document.querySelector(".cards-container");
+const searchElement = document.querySelector("#search");
+let filteredCharactersArray;
+let namesFoundCount;
+console.log(new Date().getFullYear())
+
+// Load Button Event Listener
+loadDataButtonElement.addEventListener("click", () => {
+  namesFoundCount = 0;
+  filteredCharactersArray = [];
+
+  cardContainerElement.innerHTML = "";
+  Promise.all(promises).then((pages) => {
+      pages.flatMap((page) => page.results).forEach(character => {
+          getFilteredCharacters(character)
+      })
+      if (namesFoundCount === 0) {
+        alert("No Character found with the name: " + searchElement.value)
+      } else {
+      addCardElements(filteredCharactersArray)
+      }
+})
+});
+
+function getFilteredCharacters(character) {
+  const filterElement = document.querySelector("#character-state");
+
+  // Filter by Character Status
+  if (searchElement.value === "") {
+    if (filterElement.value === "All") {
+      filteredCharactersArray.push(character);
+      namesFoundCount++
+    }
+    if (filterElement.value === "Alive") {
+      if (character.status === "Alive") {
+        filteredCharactersArray.push(character);
+        namesFoundCount++
+      }
+    }
+    if (filterElement.value === "Dead") {
+      if (character.status === "Dead") {
+        filteredCharactersArray.push(character);
+        namesFoundCount++
+      }
+    }
+    if (filterElement.value === "unknown") {
+      if (character.status === "unknown") {
+        filteredCharactersArray.push(character)
+        namesFoundCount++
+      }
+    }
+  }
+  
+  // Filter by specific name
+  if (searchElement.value !== "") {
+    if (
+      character.name.toLowerCase().includes(searchElement.value.toLowerCase())
+    ) {
+      filterElement.value = "All";
+      filteredCharactersArray.push(character);
+      namesFoundCount++;
+    }
+  }
+}
+
+// Function to add Element to CardContainer
+function addCardElements(filteredCharacters) {
+
+    const cardContainerElement = document.querySelector(".cards-container")
+
+    filteredCharacters.forEach((character) => {
+      const newCardElement = document.createElement("li");
+      newCardElement.classList.add("cards")
+      cardContainerElement.append(newCardElement)
+
+      const newImageElement = document.createElement("img")
+      newImageElement.setAttribute("src", character.image)
+      newCardElement.append(newImageElement)
+
+      const paragraphElement = document.createElement("p")
+      paragraphElement.textContent = character.name
+      newCardElement.append(paragraphElement)
+
+      const paragraphStatusElement = document.createElement("p")
+      paragraphStatusElement.textContent = character.status
+      newCardElement.append(paragraphStatusElement)
+    });
+}
